7d55f3f677366579ae4a35a0afd32b82a0337715
John Faria <johnwbf@hotmail.com>
2020-09-30 09:11:08 -0300

    Alterado comando keyscan em deploy.sh
    Alterado nome do banco de dados em config
    
    Alterado nome do banco de dados em config
    
    Adicionado antiga configuração de banco de dados
    
    Corrigido o nome do arquivo de varaiveis de ambiente

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ echo -e "${SSH_KEY}//_/\\n" > ~/.ssh/id_rsa
 
 chmod 600 ~/.ssh/id_rsa
 
-ssh_keyscan -p 9922 -t rsa geonosis.deploy.umbler.com 2>&1 >> ~/.ssh/known_hosts
+ssh-keyscan -p 9922 -t rsa geonosis.deploy.umbler.com 2>&1 >> ~/.ssh/known_hosts
